8bf489beb44c6172e64c2835ac3dbf4da43a2fe2
sravanthi palchuri <sravanthipalchuri@gmail.com>
2026-02-13 19:17:09 +0530

    Fixed NLTK path and  lookup error

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ os.makedirs(nltk_data_path, exist_ok=True)
 nltk.data.path.append(nltk_data_path)
 
 nltk.download('punkt', download_dir=nltk_data_path)
+nltk.download('punkt_tab', download_dir=nltk_data_path)
 nltk.download('stopwords', download_dir=nltk_data_path)
 
 
