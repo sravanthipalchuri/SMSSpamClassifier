5b18efb2c7dfea545ffe7c0752b604f7189a6e4b
sravanthi palchuri <sravanthipalchuri@gmail.com>
2026-02-13 19:12:49 +0530

    Fixed NLTK lookup error

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ import nltk
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
+nltk.download('punkt')
+nltk.download('stopwords')
+
 ps=PorterStemmer()
 
 def transform_text(text):
